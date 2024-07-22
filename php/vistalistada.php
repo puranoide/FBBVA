234e6228094b11e147abb5710172541856dd3584
puranoide <121548953+puranoide@users.noreply.github.com>
2024-07-22 17:17:15 -0500

    cambios de estructura
    aca se creo el boton cerrar sesion y tambien se separo las vistas de listado y busqueda con el del agregar post

--- php/vistalistada.php
+++ php/vistalistada.php
@@ -27,9 +27,6 @@ if($_SESSION['rol']==1){
               <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="viewParametroReporte.php">Agregar Parametro de valor por red social</a>
             </div>
-             <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="vistalistada.php">historial de registro</a>
-            </div>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
              <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="cerrar_sesion.php">cerrar sesion</a>
@@ -51,9 +48,6 @@ if($_SESSION['rol']==1){
              <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="vistalistada.php">historial de registro</a>
             </div>
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="cerrar_sesion.php">cerrar sesion</a>
-            </div>
             </div>
 
             
@@ -62,64 +56,68 @@ if($_SESSION['rol']==1){
     ';
 }
 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>lista</title>
     <link rel="stylesheet" href="../assets/styles.css">
-    <title>Inicio del portal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-
-    <?php
+<?php
     echo $componente;
     ?>
 
-    <br>
-
-
-    <form action="cerrar_sesion.php">
-        <button>cerrar sesion</button>
-    </form>
-<div class="conteiner-inputs-selects">
-        <select id="seleccionFundacion" onchange="actualizarFormulario()">
-            <option value="">Selecciona una fundacion</option>
-            <option value="Faromedic">Faromedic</option>
-            <option value="FBBVA">FBBVA</option>
-        </select>
-    
-        <select id="seleccion" onchange="actualizarFormulario()">
-            <option value="">Selecciona una red social</option>
-            <option value="FB">Facebook</option>
-            <option value="IG">Instagram</option>
+<div class="conteiner-inputs-selects-filtro">
+            <select id="seleccionFundacionFiltro" onchange="ActualizarForm()">
+                <option value="">Selecciona una fundacion</option>
+                <option value="Faromedic">Faromedic</option>
+                <option value="FBBVA">FBBVA</option>
+            </select>
+        
+            <select id="seleccionFiltro" onchange="ActualizarForm()">
+                <option value="">Selecciona una red social</option>
+                <option value="Facebook">Facebook</option>
+                <option value="Instagram">Instagram</option>
+                
+            </select>
             
-        </select>
-        
-        <input type="date" name="Fecha Campaña" id="dateCampania" onchange="actualizarFormulario()">
-        
-        
-        <select id="seleccionObjetivo" onchange="actualizarFormulario()">
-            <option value="">Selecciona un objetivo</option>
-            <option value="PPL">PPL</option>
-            <option value="PPA">PPA</option>
-        </select>
-        <input type="text" name="Fecha Campaña" id="NombreEscritoCampania" oninput="actualizarFormulario()">
-    </div>
-    <form id="formulario"  action="postdata.php" method="post" class="FormPost">
+            desde:<input type="date" name="Fecha Campaña" id="dateCampaniadesdeFiltro" onchange="ActualizarForm()">
+            hasta:<input type="date" name="Fecha Campaña" id="dateCampaniahastaFiltro" onchange="ActualizarForm()">
+            
+            
+            <select id="seleccionObjetivoFiltro" onchange="ActualizarForm()">
+                <option value="">Selecciona un objetivo</option>
+                <option value="PPL">PPL</option>
+                <option value="PPA">PPA</option>
+            </select>
+            <select id="seleccionnameFiltro" onchange="ActualizarForm()">
+            <option value="">seleccione por nombre</option>
+            <?php
+            include 'conection.php';
+            $query = "SELECT DISTINCT namePost FROM post ORDER BY namePost ASC";
+            $resultNombre = $conexion->query($query);
+            while($row=$resultNombre->fetch_assoc()){
+                echo "<option value='" . $row['namePost'] . "'>" . $row['namePost'] . "</option>";
+            }
+            ?>
+            </select>
+        </div>
+        <form id="formularioFiltro"  action="viewDataFilter.php" method="post" class="FormParaFiltro">
         <!-- Campos del formulario que se actualizarán dinámicamente -->
+        
     </form>
 
+<?php
+    $rutalist='ListData.php';
+    require_once $rutalist;
 
+?>
 
-
-
-
-
-
- <script src="../assets/app.js"></script>
- <script src="../assets/appFiltrar.js"></script>
+<script src="../assets/appFiltrar.js"></script>
 </body>
 </html>
